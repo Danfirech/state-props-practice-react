b3a7c82521278eec13b2283a06b837977dbb0a7a
Dan Possehl <danielbpossehl@gmail.com>
2021-11-12 11:11:54 -0700

    added constructor and super

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from "react";
+
+import Demo from "./components /Demo";
+
 function App() {
-  return <div className="App">Start</div>;
+  return (
+    <div>
+      <Demo name="Daniel" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components /Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Demo extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return <h1>Demo Component</h1>;
+  }
+}
+
+export default Demo;
